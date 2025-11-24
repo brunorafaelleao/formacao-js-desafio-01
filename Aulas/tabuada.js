4d50d09be62ab0da4145a56c65e024f18f24f76a
Bruno Leão <bruno.melosh@gmail.com>
2025-11-24 13:11:14 -0400

    final da aula sobre arrays

--- Aulas/tabuada.js
+++ Aulas/tabuada.js
@@ -2,9 +2,25 @@
 //comando: npm install prompt-sync
 prompt = require("prompt-sync")();
 let valor = prompt("Digite o valor que deseja ver a tabuada: ");
-
 for (let i = 0; i <= 10; i++) {
-  6;
   //console.log(i * valor);
   console.log(`${valor} * ${i}  = ${i * valor}`);
 }
+
+//testes de exercicios:
+
+// }
+// const numerosPares = [];
+// for (let i = 0; i < 10; i++) {
+//   const numeroPar = i % 2 === 0;
+//   if (numeroPar) {
+//     numerosPares.push(i);
+//   }
+// }
+// console.log(numerosPares);
+
+// const numerosPares = [];
+// for (let i = 0; i < 10; i = i + 2) {
+//   numerosPares.push(i);
+// }
+// console.log(numerosPares);
