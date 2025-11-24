84ebee82bae7d8fecd0688a121966f51883a8efe
Bruno Leão <bruno.melosh@gmail.com>
2025-11-24 12:58:41 -0400

    update das aulas para poder realizar o projeto (assunto: arrays)

---
+++ Aulas/tabuada.js
@@ -0,0 +1,10 @@
+//para rodar esse código, é necessário instalar o prompt-sync
+//comando: npm install prompt-sync
+prompt = require("prompt-sync")();
+let valor = prompt("Digite o valor que deseja ver a tabuada: ");
+
+for (let i = 0; i <= 10; i++) {
+  6;
+  //console.log(i * valor);
+  console.log(`${valor} * ${i}  = ${i * valor}`);
+}
